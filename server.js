b28d2e37c958e6fab8fe3e8dc7131d0939a080a1
Marilia-Savina <mariliasavina@gmail.com>
2026-04-09 18:55:09 -0300

    instalaçao de ferramentas no package.json server,js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const cors = require('cors')
 const {criarBanco} = require('./database')
-
+const cors = require('cors')
 
 const app = express()
-app.use(express.json())
 app.use (cors())
+
+app.use(express.json())
+
 
 
 // Rota teste front
